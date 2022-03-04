9bf2e393253a991388b9828f53d94acb5ccbcedd
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-03-04 20:19:22 +0530

    to show integer, real no and Charecter

---
+++ TO print real no, integer and charecter.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+	int a;
+	char b;
+	float c;
+	
+	printf ("Input an Integer\n");
+	scanf("%d",&a);
+	printf("Integer is :%d\n",a);
+	
+	fflush(stdin);
+	printf ("Input a Character\n");
+	scanf("%c",&b);
+	printf("Integer is :%c\n",a);
+	
+	fflush(stdin);	
+	printf ("Input a real numder\n");
+	scanf("%f",&c);
+	printf("Integer is :%f\n",c);
+}
