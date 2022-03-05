32486d535109ba69e43aea8af9b323804d16cede
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-03-05 08:13:18 +0530

    add two no

---
+++ add two no.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int a,b,c;
+	printf("Enter Two Integer : " );
+	scanf("%d%d",&a,&b);
+	c = a+b;
+	printf("Result = %d",c);
+	getch();
+	return 0;
+	
+}
