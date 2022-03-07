07fb5c45f2253a7394db89beccaffcfa07578f2b
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-03-07 22:23:04 +0530

    Area of square

---
+++ Area of square.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+//Area of Square
+int main () {
+	float side ;
+	printf("Enter side");
+	scanf("%f", &side);
+	printf("area is : %f", side*side);
+	return 0;
+}
