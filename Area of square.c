924f042c6106a290797e69cac41449d89868d4e5
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-03-07 22:37:21 +0530

    Area of Square

---
+++ Area of square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+ 
+ //Area of Square;
+ 
+ int main () {
+ 	float radius;
+ 	printf("Enter radius");
+ 	scanf("%f",&radius);
+ 	printf("area is : %f",3.14*radius*radius);
+ 	return 0;
+ }
