39158d26df5245e29a5675d45cb52b804bc68815
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-06-16 22:40:01 +0530

    vowel

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main ()
+{
+	char b;
+	printf("Enter an alphabet :");
+	scanf("%c",&b);
+	
+	if((b=='a')||(b=='A')||(b=='e')||(b=='E')||(b=='i')||(b=='I')||(b=='o')||(b=='O')||(b=='u')||(b=='U'))
+	{
+		printf("The input alphabet is a vowel");
+		
+	} else{
+		printf("The input alphabet is not vowel");
+	}
+	return 0;
+}
