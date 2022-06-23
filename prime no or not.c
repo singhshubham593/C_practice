44d553a7e6a833e37f9b4c04c17e78be5e56d293
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-06-23 11:41:10 +0530

    prime no or not

---
+++ prime no or not.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	int i,n,count=0;
+	printf("Enter the number :");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		if(n%i==0){
+			count++;
+		}
+		
+	}
+	if(count==2){
+		printf("prime no");
+	}
+	else{
+		printf("not prime no");
+	}
+	
+	return 0;
+}
