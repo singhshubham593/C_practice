ec0389f9afed9053775971fbbb51e514c17fa5c4
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-06-24 08:27:18 +0530

    add n number given by user

---
+++ add n number enter  by user.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int i,n,sum=0;
+	printf("Enter the number :");
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++){
+		sum=sum+i;
+	}
+	printf("Number is %d",sum);
+	return 0;
+}
