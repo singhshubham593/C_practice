656e722e63cab0c864d9dc30fcc658e781cedaf7
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-06-25 19:36:56 +0530

    Create convert decimal to binary no.c

---
+++ convert decimal to binary no.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int var,no,re,binary=0,i=0;
+	printf("Enter the number :");
+	scanf("%d",&no);
+	var=no;
+	while(no!=0){
+		re=no%2;
+		no=no/2;
+		binary=binary+(re*i);
+		i=i*10;
+	}
+	printf("binary number of %d is %d",var,binary);
+	getch();
+	return 0;
+}
