fbab80e5c7132b9773a25b48f9ab0d369138bf4e
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-06-25 20:17:20 +0530

    Create swap with variable.c

---
+++ swap with variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	int a,b,c;
+	printf("Enter the value of a and b :");
+	scanf("%d%d",&a,&b);
+	int temp;
+	
+	temp=a;
+	a=b;
+	b=temp;
+	
+	printf("swap value is a=%d ",a);
+	printf("swap value is b=%d",b);
+	return 0;
+}
