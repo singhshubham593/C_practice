af8920511bf7695e1c4113c6653c34db42fc5d61
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-06-26 12:21:14 +0530

    Create pascal triangle.c

---
+++ pascal triangle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+     int row,col,space,n,no;
+     
+    printf("Enter the no of row :");
+    scanf("%d",&no);
+    
+    for(row=0;row<=no;row++){
+    	for(space=0;space<=no-row;space++){
+    		printf(" ");
+		}
+		n=1;
+		for(col=0;col<=row;col++){
+			printf(" %d",n);
+			n=n*(row-col)/(col+1);
+		}
+		printf("\n");
+		
+	}
+	getch();
+	return 0;
+}
