99b38a7b985f297b2732472ccba2745d13a3e96b
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-07-02 21:14:34 +0530

    Update primd pattern.c

--- primd pattern.c
+++ primd pattern.c
@@ -1,4 +1,13 @@
 #include<stdio.h>
+
+/*                      *
+                       * *
+                      * * *
+					 * * * *
+					* * * * * 
+				   * * * * * * 
+				                      */	
+									  /*    
  
 int main(){
 	int i,j,n=6;
@@ -9,6 +18,29 @@ int main(){
 		}
 		for(j=1;j<=i;j++){
 			printf("* ");
+		}
+		
+		printf("\n");
+	}
+	return 0;
+} */
+     /*          *
+                ***
+			   *****
+			  *******
+			 ********* 
+			                     */
+
+
+int main(){
+	int i,j,n=5;
+	
+	for(i=1;i<=n;i++){
+		for(j=n-i;j>=0;j--){
+			printf(" ");
+		}
+		for(j=1;j<=2*i-1;j++){
+			printf("*");
 		}
 		
 		printf("\n");
