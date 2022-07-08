4911615c0700b4669b7c3d75eb950ca97bdc3c5b
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-07-08 22:00:09 +0530

    Create sum_of_n_prime_no_given_by_user.c

---
+++ sum_of_n_prime_no_given_by_user.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	int n,i,sum=0,j;
+	printf("Enter the number :");
+	scanf("%d",&n);
+	
+	for(j=2;j<=n;j++){
+		for(i=2;i<j;i++){
+	   		if(j%i==0)
+			break;
+	    }
+		if(i==j)
+		{
+		 
+		   printf("%d ",i);
+		   sum=sum+i;
+	     }
+	}
+	printf("\n Sum of all %d prime number is %d",n,sum);
+}
