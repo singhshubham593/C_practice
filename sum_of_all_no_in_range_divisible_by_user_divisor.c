6fa8f69ab166dda18983959e99a58bb9b4cac6ca
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-07-08 17:19:36 +0530

    Create sum_of_all_no_in_range_divisible_by_user_divisor.c

---
+++ sum_of_all_no_in_range_divisible_by_user_divisor.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+int main(){
+	int i;
+	int initial;
+	int final;
+	int user=5;
+	int sum=0;
+	
+	printf("Enter the range :\n");
+	scanf("%d %d",&initial,&final);
+	printf("Enter the diviser:");
+	scanf("%d",&user);
+	
+	for(i=initial;i<final;i++){
+		if(i%user==0){
+			printf("%d ",i);
+			sum=sum+i;
+		}
+	}
+	printf("\n\n The sum of no is :%d\n",sum);
+}
