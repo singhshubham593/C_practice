3f791380078e8c379c8112623afa4aa80516241f
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-07-15 21:27:30 +0530

    Update fibonacci series up to nth term..c

--- fibonacci series up to nth term..c
+++ fibonacci series up to nth term..c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
-
-
+// fibonacci series up to nth term from loop.
+/*
 int main(){
 	int i,n,f1,f2,f3;
 	printf("Enter tne number :");
@@ -16,5 +16,33 @@ int main(){
     f1=f2;
     f2=f3;
   }
-	
+  
 }
+  */
+   // fabonacci series up to nth term from recursion.
+   
+   #include<stdio.h>
+    
+    int fibo(int n);
+    
+    int main(){
+    	
+    	fibo(6);
+    	return 0;
+	}
+	
+	int fibo(int n){
+	    	if(n == 0){
+			 return 0;
+		    }
+	     	if(n == 1){
+	  	    	return 1;
+	      	}
+		
+		int fib1 = fib(n-1);
+		int fib2 = fib(n-2);
+		int fibn = fib1 + fib2;
+		printf("fib of %d is : %d\n",n,fibn);
+		return fibn;
+	}
+
