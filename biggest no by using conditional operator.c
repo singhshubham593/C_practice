774f8c2e0f2dec459e65206f93e2531b5aa3f8c8
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-07-17 21:33:35 +0530

    C : biggest no by using conditional operator

---
+++ biggest no by using conditional operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+// biggest of 3 no using conditional operator;
+
+int main(){
+	int a,b,c;
+	
+	printf("Enter no an,b and c");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("\n");
+	
+	// condition?expression1:expression2;
+	(a>b && a>c)?printf("%d",a):(b>c)?printf("%d",b):printf("%d",c);
+}
