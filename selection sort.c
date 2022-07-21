24a5c2e8099d66b7326a2a8b5813ca062be09f97
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-07-21 23:25:10 +0530

    Create selection sort.c

---
+++ selection sort.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	int i,j,n,temp,min;
+	int a[i];
+	
+	printf("Enter the Element :");
+	Scanf("%d",&n);
+	for(i=0;i<n-1;i++){
+		min=i;
+		for(j=i+1;j<n;i++){
+			if(a[j]<a[min]){
+				min=j;
+			}
+		}
+		temp=a[i];
+		a[i]=a[min];
+		a[min]=temp;
+	}
+	printf("%d",a[min]);
+	return 0;
+}
