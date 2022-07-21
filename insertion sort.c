f141dc3f7dad8fc0e508cf7261dd83385a21d764
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-07-21 23:46:11 +0530

    Create insertion sort.c

---
+++ insertion sort.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int i,j,temp;
+	int a[6]={7,3,5,4,2,6};
+	for(i=1;i<5;i++){
+		temp=a[i];
+		j=i-1;
+		while(j>=0 && a[j]>temp){
+			a[j+1]=a[j];
+			j=j-1;
+		}
+		a[j+1]=temp;
+	}
+	printf("%d",a[i]);
+	return 0;
+}
